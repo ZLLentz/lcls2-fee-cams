c736764381660995c86d93ff8fa0e28a1c7a076d
Zachary Lentz <zlentz@slac.stanford.edu>
2020-06-09 19:05:29 -0700

    ENH: clean up script to properly stage camera

--- scripts/save_image_direct.py
+++ scripts/save_image_direct.py
@@ -1,15 +1,43 @@
 import sys
+import time
 
-from PIL import Image
+from ophyd.areadetector.detectors import DetectorBase
+from ophyd.areadetector.cam import CamBase
 from ophyd.areadetector.plugins import ImagePlugin
+from ophyd.device import Component as Cpt
+from ophyd.log import config_ophyd_logging
+from PIL import Image
+
+
+class ScreenShot(DetectorBase):
+    cam = Cpt(CamBase, ':CAM:')
+    image = Cpt(ImagePlugin, ':CAM:DATA1:')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.stage_sigs[self.cam.acquire] = 1
 
 
 def main(pvbase, filename):
-    image_plugin = ImagePlugin(pvbase + ':CAM:DATA1:',
-                               name='image_plugin')
-    image = Image.fromarray(image_plugin.image)
+    # config_ophyd_logging(level=20)
+    print('Connecting to PVs')
+    screenshot = ScreenShot(pvbase, name='screenshot')
+    print('Starting image stream')
+    try:
+        screenshot.stage()
+        time.sleep(0.25)
+        print('Getting image')
+        image = Image.fromarray(screenshot.image.image)
+    except Exception:
+        raise
+    finally:
+        print('Cleaning up')
+        screenshot.unstage()
+    print('Saving image')
     image.save(filename)
+    print('Showing image')
     image.show()
+    print('Exiting')
 
 
 if __name__ == '__main__':
