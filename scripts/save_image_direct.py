b12403b4016d12b3c81192f256aafa577925d2c2
Zachary Lentz <zlentz@slac.stanford.edu>
2020-07-01 16:51:43 -0700

    ENH: get the image saver to work

--- scripts/save_image_direct.py
+++ scripts/save_image_direct.py
@@ -1,21 +1,36 @@
 import sys
 import time
 
+import imageio
+import numpy as np
 from ophyd.areadetector.detectors import DetectorBase
 from ophyd.areadetector.cam import CamBase
 from ophyd.areadetector.plugins import ImagePlugin
 from ophyd.device import Component as Cpt
+from ophyd.signal import EpicsSignalRO
 from ophyd.log import config_ophyd_logging
-from PIL import Image
 
 
 class ScreenShot(DetectorBase):
     cam = Cpt(CamBase, ':CAM:')
     image = Cpt(ImagePlugin, ':CAM:DATA1:')
+    bit_depth = Cpt(EpicsSignalRO, ':CAM:BitsPerPixel_RBV')
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.stage_sigs[self.cam.acquire] = 1
+
+
+def get_image(screenshot):
+    # Set type based on bit depth
+    bit_depth = screenshot.bit_depth.get()
+    image = screenshot.image.image
+    if bit_depth <= 8:
+        # Use 8-bit pixels
+        return (image * 2**8/image.max()).astype(np.uint8)
+    else:
+        # Use 16-bit pixels
+        return (image * 2**16/image.max()).astype(np.uint16)
 
 
 def main(pvbase, filename):
@@ -27,16 +42,14 @@ def main(pvbase, filename):
         screenshot.stage()
         time.sleep(0.25)
         print('Getting image')
-        image = Image.fromarray(screenshot.image.image)
+        image = get_image(screenshot)
     except Exception:
         raise
     finally:
         print('Cleaning up')
         screenshot.unstage()
     print('Saving image')
-    image.save(filename)
-    print('Showing image')
-    image.show()
+    imageio.imwrite(filename, image)
     print('Exiting')
 
 
